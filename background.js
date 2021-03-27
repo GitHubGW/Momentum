39f2d31f528177fde5cdd49b3a6db03f85a14e30
GW <kowonp@gmail.com>
2021-03-27 19:40:00 +0900

    Change Images differently on PC and on Mobile

--- background.js
+++ background.js
@@ -1,11 +1,24 @@
 const body = document.querySelector("body");
 const IMAGE_NUMBER = 6;
 
+// 접속한 기기가 PC인지 모바일인지 구분하는 함수
+// 모바일이라면 true를 PC라면 false를 반환한다.
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 function handleImgLoad() {
   console.log("Finished loading");
 }
 
-function paintImage(imgNumber) {
+function paintImagePC(imgNumber) {
+  const image = new Image();
+  console.log(image);
+  image.src = `images/background${imgNumber}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
+}
+function paintImageMobile(imgNumber) {
   const image = new Image();
   // console.log(image);
   image.src = `images/background${imgNumber}.jpeg`;
@@ -20,6 +33,12 @@ function genRandom() {
 
 function init() {
   const randomNumber = genRandom();
-  paintImage(randomNumber);
+  const checkMobile = isMobile();
+
+  if (checkMobile === true) {
+    paintImageMobile(randomNumber);
+  } else {
+    paintImagePC(randomNumber);
+  }
 }
 init();
