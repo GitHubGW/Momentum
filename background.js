55173ade49a9071c162506ab9cb7f1eabbfe99cc
GW <kowonp@gmail.com>
2021-03-27 18:47:01 +0900

    Update background

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
-const body=document.querySelector('body');
-const IMAGE_NUMBER=5;
+const body = document.querySelector("body");
+const IMAGE_NUMBER = 6;
 
-function handleImgLoad(){
+function handleImgLoad() {
   console.log("Finished loading");
 }
 
-function paintImage(imgNumber){
-  const image=new Image();
+function paintImage(imgNumber) {
+  const image = new Image();
   // console.log(image);
-  image.src=`images/background${imgNumber}.jpg`;
+  image.src = `images/background${imgNumber}.jpeg`;
   image.classList.add("bgImage");
   body.appendChild(image);
 }
 
-function genRandom(){
-  const number= Math.floor(Math.random()*IMAGE_NUMBER)+1;
+function genRandom() {
+  const number = Math.floor(Math.random() * IMAGE_NUMBER) + 1;
   return number;
 }
 
-function init(){
-  const randomNumber=genRandom();
+function init() {
+  const randomNumber = genRandom();
   paintImage(randomNumber);
 }
 init();
